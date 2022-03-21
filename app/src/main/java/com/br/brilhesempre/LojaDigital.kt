5213c173e3444a5538cce56227cbe153fa8734ee
Luciano Daniel Amarante <lucianoa2028@gmail.com>
2022-03-21 06:27:27 -0300

    Segundo Beta Brilhe Sempre
    1.0 - Tela FullscreenActivity.kt
    1.2 - Conteudo do container: Text, Button,
    2.0 - Inserção de IMG logo;
    3.0 - Testos novos;
    4.0 - Trocando corres de fundo e button;
    4.1 - Usando a Biblioteca Intent para chamar a nova tela;
    5.0 - Inserção da tela Redes Sociais;
    6.0 - Inserção Tela Redes Sociais;
    6.1 - BackGrande #EFB252;
    6.2 - Button Proximo;
    6.3 - Grade de Redes Sociais;
    6.4 - Icones de de redes sociais;
    6.5 - Texto novo ...:: Encontre nossas rede sociais;
    7.0 - Inserção de  Tela Loja Digital.

---
+++ app/src/main/java/com/br/brilhesempre/LojaDigital.kt
@@ -0,0 +1,11 @@
+package com.br.brilhesempre
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LojaDigital : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loja_digital)
+    }
+}
