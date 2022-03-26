10f5bc9e823ce01d30ac921752c9a26838e72162
Luciano Daniel Amarante <lucianoa2028@gmail.com>
2022-03-26 19:02:12 -0300

    Segundo Beta Brilhe Sempre
    1.0 - Tela FullscreenActivity.kt
    1.2 - Conteudo do container: Text, Button,
    2.0 - Inserção de IMG logo;
    3.0 - Testos novos;
    4.0 - Trocando corres de fundo e button;
    4.1 - Usando a Biblioteca Intent para chamar a nova tela;
    5.0 - Inserção da tela Redes Sociais;
    6.0 - Inserção Tela Redes Sociais;
    6.1 - BackGrande #EFB252;
    6.2 - Button Proximo;
    6.3 - Grade de Redes Sociais;
    6.4 - Icones de de redes sociais;
    6.5 - Texto novo ...:: Encontre nossas rede sociais;
    7.0 - Inserção de  Tela Loja Digital;
    8.0 - Fazendo uso da Biblioteca Inten;
    8.1 - Button Abilitado para onclick.

--- app/src/main/java/com/br/brilhesempre/RedesSociais.kt
+++ app/src/main/java/com/br/brilhesempre/RedesSociais.kt
@@ -1,11 +1,24 @@
 package com.br.brilhesempre
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RedesSociais : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_redes_sociais)
+
+        // Button Loja Digital
+        // A variavél BTNLOJADIGITAL vai receber a chamada do SetOnclickListener
+        // /////////////////
+        val btnLojaDigital = findViewById<Button>(R.id.btnProximo) as Button
+
+        btnLojaDigital.setOnClickListener {
+            val intentlojadigital = Intent(this, LojaDigital::class.java).apply {
+            }
+            startActivity(intentlojadigital)
+        }
     }
 }
